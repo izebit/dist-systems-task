be23403801e79f96dbafde49c9b3644600ae93a6
Artem Konovalov <izebit@gmail.com>
2022-10-28 10:18:33 +0300

    create structure of the project

---
+++ first-service/src/main/java/ru/izebit/first_service/ApplicationLauncher.java
@@ -0,0 +1,7 @@
+package ru.izebit.first_service;
+
+public class ApplicationLauncher {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ second-service/src/main/java/ru/izebit/second_service/ApplicationLauncher.java
@@ -0,0 +1,8 @@
+package ru.izebit.second_service;
+
+
+public class ApplicationLauncher {
+    public static void main(String[] args) {
+
+    }
+}
